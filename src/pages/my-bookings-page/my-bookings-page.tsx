25ad0b8ce97874f9040f2b70801645e65a17e188
YuriyMorkovnik <yurim@wix.com>
2024-05-06 17:54:45 +0300

    MyBookingsPage WIP

--- src/pages/my-bookings-page/my-bookings-page.tsx
+++ src/pages/my-bookings-page/my-bookings-page.tsx
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import classNames from 'classnames';
 import styles from './products-page.module.scss';
 import { Link } from 'react-router-dom';
@@ -6,14 +7,40 @@ import { ProductCard } from '../../components/product-card/product-card';
 import { useUpcomingBookings } from '../../api/api-hooks';
 import { getImageHttpUrl } from '../../api/wix-image';
 import commonStyles from '../../styles/common-styles.module.scss';
+import { TabItem, Tabs } from '/src/components/tabs/tabs';
 
 export interface ProductsPageProps {
     className?: string;
 }
 
-export const ProductsPage = ({ className }: ProductsPageProps) => {
-    const { data: myUpcomingBookings, isLoading } = useUpcomingBookings();
 
+const Booking = ({ booking }: { booking: any }) => {
+    return (
+        <div>
+            <div className={styles.date}>
+                <div className={styles.day}>
+
+                </div>
+                <div className={styles.dayInfo}>
+
+                </div>
+            </div>
+            <div className={styles.description}>
+                <div className={styles.label}>
+
+                </div>
+                <div className={styles.fullDate}>
+
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export const MyBookingsPage = ({ className }: ProductsPageProps) => {
+    const [activeTab, setActiveTab] = useState('myBookings');
+    const { data: myUpcomingBookings, isLoading } = useUpcomingBookings();
+    console.log('!', myUpcomingBookings);
     if (!myUpcomingBookings && isLoading) {
         return <div className={commonStyles.loading}>Loading...</div>;
     }
@@ -21,25 +48,13 @@ export const ProductsPage = ({ className }: ProductsPageProps) => {
     return (
         <div className={classNames(styles.root, className)}>
             <h1 className={styles.title}>All Products</h1>
-            <div className={styles.gallery}>
-                {myLessons?.map(
-                    (item) =>
-                        item.mainSlug?.name &&
-                        item.name && (
-                            <Link to={ROUTES.product.to(item.mainSlug.name)} key={item.mainSlug?.name}>
-                                <ProductCard
-                                    imageUrl={getImageHttpUrl(
-                                        item.media?.items?.at(0)?.image,
-                                        240
-                                    )}
-                                    name={item.name}
-                                    price={undefined}
-                                    className={styles.productCard}
-                                />
-                            </Link>
-                        )
-                )}
-            </div>
+            <Tabs items={[{ label: 'My Bookings', id: 'myBookings' }, { label: 'Booking history', id: 'bookingHistory' }]}>
+                <TabItem id="myBookings">
+                    {myUpcomingBookings?.map((booking) => (
+                        <Booking booking={booking} />
+                    ))}
+                </TabItem>
+            </Tabs>
         </div>
     );
 };
