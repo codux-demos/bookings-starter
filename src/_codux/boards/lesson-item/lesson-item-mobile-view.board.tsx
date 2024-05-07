e7200d13d36d2a83f0b59c572b614086c7f2f3c2
idango10 <idango@wix.com>
2024-05-07 16:32:49 +0300

    fix: `lesson-item` boards

--- src/_codux/boards/lesson-item/lesson-item-mobile-view.board.tsx
+++ src/_codux/boards/lesson-item/lesson-item-mobile-view.board.tsx
@@ -1,12 +1,15 @@
 import { LessonItem } from '../../../components/lesson-item/lesson-item';
 import { ContentSlot, createBoard } from '@wixc3/react-board';
+import { ComponentWrapper } from '../../board-wrappers/component-wrapper';
 
 export default createBoard({
     name: 'Lesson Item Mobile View',
     Board: () => (
-        <ContentSlot>
-            <LessonItem price="$15" title="Pump It Up" link="" />
-        </ContentSlot>
+        <ComponentWrapper settings={{}}>
+            <ContentSlot>
+                <LessonItem price="$15" title="Pump It Up" link="" />
+            </ContentSlot>
+        </ComponentWrapper>
     ),
     isSnippet: true,
     environmentProps: {
