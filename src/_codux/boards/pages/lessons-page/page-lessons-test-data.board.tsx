25a57d4cdcc7287b999e1e13c8021d842a650d4e
idango10 <idango@wix.com>
2024-05-07 20:09:09 +0300

    refactor: rename products-page to lessons-page

--- src/_codux/boards/pages/lessons-page/page-lessons-test-data.board.tsx
+++ src/_codux/boards/pages/lessons-page/page-lessons-test-data.board.tsx
@@ -1,20 +1,20 @@
-import { ProductsPage } from '../../../../pages/products-page/products-page';
+import { LessonsPage } from '../../../../pages/lessons-page/lessons-page';
 import { ContentSlot, createBoard } from '@wixc3/react-board';
 import { PageWrapper } from '/src/_codux/board-wrappers/page-wrapper';
 import { waitForProductImage } from '/src/_codux/board-wrappers/wait-for-helpers';
 
 export default createBoard({
-    name: 'Test Products Gallery Page',
+    name: 'Test Lessons Page',
     Board: () => (
         //in practice PageWrapper with a path will render the correct page, but it is less convenient to use in a board
         <PageWrapper
-            path="/products"
+            path="/lessons"
             settings={{
                 imagesListToLoop: 'all',
             }}
         >
             <ContentSlot>
-                <ProductsPage />
+                <LessonsPage />
             </ContentSlot>
         </PageWrapper>
     ),
--- src/pages/home-page/home-page.tsx
+++ src/pages/home-page/home-page.tsx
@@ -2,8 +2,7 @@ import classNames from 'classnames';
 import styles from './home-page.module.scss';
 import { HeroImage } from './hero-image/hero-image';
 import { ROUTES } from '../../router/config';
-import { Link, useNavigate } from 'react-router-dom';
-import { ProductCard } from '../../components/product-card/product-card';
+import { useNavigate } from 'react-router-dom';
 import { usePromotedLessons } from '../../api/api-hooks';
 
 export interface HomePageProps {
@@ -23,18 +22,18 @@ export const HomePage = ({ className }: HomePageProps) => {
                 bottomLabel="Get more for less on selected brands"
                 buttonLabel="Shop Now"
                 topLabelClassName={styles['top-label-highlighted']}
-                onButtonClick={() => navigate(ROUTES.products.to())}
+                onButtonClick={() => navigate(ROUTES.lessons.to())}
             />
             <h1 className={styles['hero-title']}>Best Sellers</h1>
             <p className={styles.HPprgrp}>Shop our best seller items</p>
             {/* <div className={styles.cardsLayout}>
-                {products?.map((product) =>
-                    product.slug && product.name ? (
-                        <Link to={ROUTES.product.to(product.slug)} key={product.slug}>
+                {lessons?.map((lesson) =>
+                    lesson.slug && lesson.name ? (
+                        <Link to={ROUTES.lesson.to(lesson.slug)} key={lesson.slug}>
                             <ProductCard
-                                imageUrl={product.media?.items?.at(0)?.image?.url}
-                                name={product.name}
-                                price={product.price ?? undefined}
+                                imageUrl={lesson.media?.items?.at(0)?.image?.url}
+                                name={lesson.name}
+                                price={lesson.price ?? undefined}
                                 className={styles.productCard}
                             />
                         </Link>
