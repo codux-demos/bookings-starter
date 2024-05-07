cf420b648af745fb47811da9dbe9d776351cb7e5
idango10 <75563024+idango10@users.noreply.github.com>
2024-05-07 16:25:58 +0300

    feat: lessons page (#7)

---
+++ src/_codux/boards/lesson-item/lesson-item.board.tsx
@@ -0,0 +1,17 @@
+import { LessonItem } from '../../../components/lesson-item/lesson-item';
+import { ContentSlot, createBoard } from '@wixc3/react-board';
+
+export default createBoard({
+    name: 'Lesson Item',
+    Board: () => (
+        <ContentSlot>
+            <LessonItem price="$15" title="Pump It Up" link="" />
+        </ContentSlot>
+    ),
+    isSnippet: true,
+    environmentProps: {
+        canvasMargin: { right: 0, bottom: 0, left: 0, top: 0 },
+        windowWidth: 1024,
+        windowHeight: 768,
+    },
+});
---
+++ src/components/lesson-item/lesson-item.tsx
@@ -0,0 +1,26 @@
+import classNames from 'classnames';
+import { Link } from 'react-router-dom';
+import { ROUTES } from '../../router/config';
+import styles from './lesson-item.module.scss';
+import CommonStyles_module from '../../styles/common-styles.module.scss';
+
+export interface LessonItemProps {
+    title: string;
+    price: string;
+    link: string;
+    className?: string;
+}
+
+export const LessonItem = ({ title, price, link, className }: LessonItemProps) => {
+    return (
+        <div className={classNames(styles.root, className)}>
+            <h1 className={styles.title}>{title}</h1>
+            <span className={CommonStyles_module.price}>{price}</span>
+            <Link className={styles.bookNow} to={ROUTES.product.to(link)}>
+                <button className={CommonStyles_module.primaryButton}>
+                    Book Now
+                </button>
+            </Link>
+        </div>
+    );
+};
