dc45cd1b671ed956691def65a70bc47893f854ee
Daniel Cohen <danielcoh@wix.com>
2024-05-19 09:52:05 +0300

    changes to new data representation

--- src/_codux/boards/calendar/calendar.board.tsx
+++ src/_codux/boards/calendar/calendar.board.tsx
@@ -3,28 +3,69 @@ import { ContentSlot, createBoard } from '@wixc3/react-board';
 import { ComponentWrapper } from '/src/_codux/board-wrappers/component-wrapper';
 import Calendar_board_module from './calendar.board.module.scss';
 
-const data = {
-    '01/05/2024': ['10:00', '11:00', '12:00'],
-    '02/05/2024': ['10:20', '11:40', '12:00'],
-    '07/05/2024': ['13:00', '14:40'],
-    '08/05/2024': ['10:00', '11:00', '12:00'],
-    '13/05/2024': ['10:00'],
-    '16/05/2024': ['10:00'],
-    '17/05/2024': ['10:00'],
-};
+const mockData = [
+    {
+        "slot": {
+            "sessionId": "bd299248-dc61-44de-9258-56c33ae0ec58",
+            "serviceId": "fd9d0ea6-642b-490b-a0a0-318b8ebf5fc7",
+            "scheduleId": "e28d042c-c478-47d5-bd4e-f56de9acd5d2",
+            "startDate": "2024-05-16T20:21:27.649Z",
+            "endDate": "2024-05-16T21:21:27.649Z"
+        },
+        "bookable": true,
+        "isFromV2": false,
+        "locked": false,
+        "openSpots": 15,
+        "totalSpots": 15,
+        "waitingList": {}
+    },
+    {
+        "slot": {
+            "sessionId": "ab3ccb79-db0b-4000-a8bc-b3a7e6d6b198",
+            "serviceId": "fd9d0ea6-642b-490b-a0a0-318b8ebf5fc7",
+            "scheduleId": "7baa8707-0cb0-4b1f-a8e7-72af7c356c1a",
+            "startDate": "2024-05-17T22:04:04.446Z",
+            "endDate": "2024-05-17T23:04:04.446Z"
+        },
+        "bookable": true,
+        "isFromV2": false,
+        "locked": false,
+        "openSpots": 15,
+        "totalSpots": 15,
+        "waitingList": {}
+    },
+    {
+        "slot": {
+            "sessionId": "83972fd0-e224-4cff-aef2-cf6c183e1ee6",
+            "serviceId": "fd9d0ea6-642b-490b-a0a0-318b8ebf5fc7",
+            "scheduleId": "a8a6502a-7248-458f-bb74-a658f5a53213",
+            "startDate": "2024-05-20T02:55:29.458Z",
+            "endDate": "2024-05-20T03:55:29.458Z"
+        },
+        "bookable": true,
+        "isFromV2": false,
+        "locked": false,
+        "openSpots": 15,
+        "totalSpots": 15,
+        "waitingList": {}
+    }
+]
+
 
 export default createBoard({
     name: 'Calendar',
 
-    Board: () => (
-        <ComponentWrapper settings={{}}>
+
+    Board: () => {
+
+          <ComponentWrapper settings={{}}>
             <ContentSlot>
                 <div className={Calendar_board_module.heyyyy}>
-                    <Calendar data={data} />
+                    <Calendar data = {mockData} />
                 </div>
             </ContentSlot>
         </ComponentWrapper>
-    ),
+    },
 
     isSnippet: true,
     environmentProps: {
--- src/components/calendar/calendar.tsx
+++ src/components/calendar/calendar.tsx
@@ -4,15 +4,23 @@ import DatePicker from 'react-datepicker';
 import 'react-datepicker/dist/react-datepicker.css';
 import { CalendarDate } from '../calendar-date/calendar-date';
 import './datepickerstyles.css';
+import { useParams } from 'react-router-dom';
+import { RouteParams } from '/src/router/config';
+import { useAvailability, useLessonBySlug } from '/src/api/api-hooks';
 
 interface CalendarProps {
-    data: { [date: string]: string[] }; // Dates are in 'DD/MM/YYYY' format
+    data: { [date: string]: string[] };
 }
 
-export const Calendar: React.FC<CalendarProps> = ({ data }) => {
+export const Calendar: React.FC<CalendarProps> = () => {
+    const { slug } = useParams<RouteParams['/lesson/:slug']>();
+    const { data } = useLessonBySlug(slug);
+    const { data: availability, isLoading } = useAvailability(data?._id!);
+    console.log(availability)
+
+
     const [selectedDate, setSelectedDate] = useState(new Date());
     const today = startOfDay(new Date());
-
     const renderDayContents = (day: number, date: Date) => {
         const dateString = format(date, 'dd/MM/yyyy');
         const isAvailable = dateString in data && date >= today;
