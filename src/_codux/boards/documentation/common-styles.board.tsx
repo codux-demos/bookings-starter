c4a6efa64514c1d6334d4841486b0d129c354a37
Daniel Cohen <danielcoh@wix.com>
2024-05-19 15:48:30 +0300

    removed timeslot button

--- src/_codux/boards/documentation/common-styles.board.tsx
+++ src/_codux/boards/documentation/common-styles.board.tsx
@@ -22,7 +22,7 @@ export default createBoard({
         canvasMargin: {
             top: 0,
             left: 0,
-            bottom: 39,
+            bottom: 0,
             right: 0,
         },
         windowWidth: 1024,
