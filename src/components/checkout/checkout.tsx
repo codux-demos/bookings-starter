1c87bd643d9365d1e07799cb459df5aedd4f5212
YuriyMorkovnik <yurim@wix.com>
2024-05-19 15:55:14 +0300

    css improvements

--- src/components/checkout/checkout.tsx
+++ src/components/checkout/checkout.tsx
@@ -10,31 +10,33 @@ export interface CheckoutProps {
 export const Checkout = ({ className }: CheckoutProps) => {
     return (
         <div className={classNames(styles.root, className)}>
-            <h6 className={styles.title}>Customer details </h6>
+            <h6 className={styles.title}>Client details</h6>
             <form>
-                <label className={commonStyles['input-label']} htmlFor="email">
-                    Email
-                    <input
-                        id="email"
-                        type="email"
-                        required
-                        aria-required
-                        pattern="^.+@.+\.[a-zA-Z]{2,63}$"
-                        name="email"
-                        maxLength={150}
-                    />
-                </label>
-                <label className={commonStyles['input-label']} htmlFor="first-name">
-                    First name
-                    <input
-                        id="first-name"
-                        type="text"
-                        aria-required
-                        required
-                        name="name"
-                        maxLength={100}
-                    />
-                </label>
+                <div className={styles.section}>
+                    <label className={commonStyles['input-label']} htmlFor="first-name">
+                        Name
+                        <input
+                            id="first-name"
+                            type="text"
+                            aria-required
+                            required
+                            name="name"
+                            maxLength={100}
+                        />
+                    </label>
+                    <label className={commonStyles['input-label']} htmlFor="email">
+                        Email
+                        <input
+                            id="email"
+                            type="email"
+                            required
+                            aria-required
+                            pattern="^.+@.+\.[a-zA-Z]{2,63}$"
+                            name="email"
+                            maxLength={150}
+                        />
+                    </label>
+                </div>
                 <label className={commonStyles['input-label']} htmlFor="last-name">
                     Last name
                     <input id="last-name" type="text" name="lastName" maxLength={150} />
@@ -45,11 +47,21 @@ export const Checkout = ({ className }: CheckoutProps) => {
                 </label>
                 <label className={commonStyles['input-label']} htmlFor="phone">
                     Add your message
-                    <input id="message" type="text" name="message" maxLength={200} />
+                    <input
+                        id="message"
+                        type="text"
+                        name="message"
+                        maxLength={200}
+                        multiple
+                        aria-rowcount={0}
+                    />
                 </label>
                 <button
-                    type='submit'
-                    className={classNames(CommonStyles_module.primaryButton, styles['checkout-button'])}
+                    type="submit"
+                    className={classNames(
+                        CommonStyles_module.primaryButton,
+                        styles['checkout-button'],
+                    )}
                 >
                     Continue
                 </button>
