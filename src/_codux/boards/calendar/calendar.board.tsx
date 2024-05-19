6d2cb9600c0d34fc4679309579e6b8a82b4f603e
Daniel Cohen <danielcoh@wix.com>
2024-05-19 14:33:15 +0300

    changed the Calendar Component to use the context instead of props

--- src/_codux/boards/calendar/calendar.board.tsx
+++ src/_codux/boards/calendar/calendar.board.tsx
@@ -3,75 +3,6 @@ import { ContentSlot, createBoard } from '@wixc3/react-board';
 import { ComponentWrapper } from '/src/_codux/board-wrappers/component-wrapper';
 import Calendar_board_module from './calendar.board.module.scss';
 
-const mockData = {
-    "16/05/2024": [
-        {
-            "slot": {
-                "sessionId": "bd299248-dc61-44de-9258-56c33ae0ec58",
-                "serviceId": "fd9d0ea6-642b-490b-a0a0-318b8ebf5fc7",
-                "scheduleId": "e28d042c-c478-47d5-bd4e-f56de9acd5d2",
-                "startDate": "2024-05-16T20:21:27.649Z",
-                "endDate": "2024-05-16T21:21:27.649Z"
-            },
-            "bookable": true,
-            "isFromV2": false,
-            "locked": false,
-            "openSpots": 15,
-            "totalSpots": 15,
-            "waitingList": {}
-        },
-        {
-            "slot": {
-                "sessionId": "d9f8cbcb-dc61-44de-9258-56c33ae0ec59",
-                "serviceId": "fd9d0ea6-642b-490b-a0a0-318b8ebf5fc7",
-                "scheduleId": "e28d042c-c478-47d5-bd4e-f56de9acd5d2",
-                "startDate": "2024-05-16T20:21:27.649Z",
-                "endDate": "2024-05-16T21:21:27.649Z"
-            },
-            "bookable": true,
-            "isFromV2": false,
-            "locked": false,
-            "openSpots": 15,
-            "totalSpots": 15,
-            "waitingList": {}
-        }
-    ],
-    "17/05/2024": [
-        {
-            "slot": {
-                "sessionId": "ab3ccb79-db0b-4000-a8bc-b3a7e6d6b198",
-                "serviceId": "fd9d0ea6-642b-490b-a0a0-318b8ebf5fc7",
-                "scheduleId": "7baa8707-0cb0-4b1f-a8e7-72af7c356c1a",
-                "startDate": "2024-05-17T22:04:04.446Z",
-                "endDate": "2024-05-17T23:04:04.446Z"
-            },
-            "bookable": true,
-            "isFromV2": false,
-            "locked": false,
-            "openSpots": 15,
-            "totalSpots": 15,
-            "waitingList": {}
-        }
-    ],
-    "20/05/2024": [
-        {
-            "slot": {
-                "sessionId": "83972fd0-e224-4cff-aef2-cf6c183e1ee6",
-                "serviceId": "fd9d0ea6-642b-490b-a0a0-318b8ebf5fc7",
-                "scheduleId": "a8a6502a-7248-458f-bb74-a658f5a53213",
-                "startDate": "2024-05-20T02:55:29.458Z",
-                "endDate": "2024-05-20T03:55:29.458Z"
-            },
-            "bookable": true,
-            "isFromV2": false,
-            "locked": false,
-            "openSpots": 15,
-            "totalSpots": 15,
-            "waitingList": {}
-        }
-    ]
-}
-
 
 
 export default createBoard({
@@ -80,7 +11,7 @@ export default createBoard({
         <ComponentWrapper settings={{}}>
             <ContentSlot>
                 <div className={Calendar_board_module.heyyyy}>
-                    <Calendar data={mockData} />
+                    <Calendar/>
                 </div>
             </ContentSlot>
         </ComponentWrapper>
--- src/components/calendar/calendar.tsx
+++ src/components/calendar/calendar.tsx
@@ -1,21 +1,25 @@
-import { format, startOfDay, isSameMonth } from 'date-fns';
-import React, { useState } from 'react';
+import { format, startOfDay, parseISO } from 'date-fns';
+import React, { useState, useContext } from 'react';
 import DatePicker from 'react-datepicker';
 import 'react-datepicker/dist/react-datepicker.css';
 import { CalendarDate } from '../calendar-date/calendar-date';
 import './datepickerstyles.css';
-
+import { useParams } from 'react-router-dom';
+import { RouteParams } from '/src/router/config';
+import { useAvailability, useLessonBySlug } from '/src/api/api-hooks';
+import { WixAPIContext } from '/src/api/wix-api-context-provider';
+import { SlotAvailability } from '@wix/redirects/build/cjs/src/headless-v1-redirect-session.universal';
 
 interface Slot {
-    sessionId: string;
-    serviceId: string;
-    scheduleId: string;
-    startDate: string;
-    endDate: string;
+    sessionId?: string | null;
+    serviceId?: string | null;
+    scheduleId?: string | null;
+    startDate?: string | null;
+    endDate?: string | null;
 }
 
 interface DataItem {
-    slot: Slot;
+    slot?: Slot;
     bookable: boolean;
     isFromV2: boolean;
     locked: boolean;
@@ -24,20 +28,30 @@ interface DataItem {
     waitingList: Record<string, unknown>;
 }
 
-interface CalendarProps {
-    data: {
-        [key: string]: DataItem[];
-    }
-}
-
-
-export const Calendar: React.FC<CalendarProps> = ({ data }) => {
+export const Calendar: React.FC = () => {
     const [selectedDate, setSelectedDate] = useState(new Date());
     const today = startOfDay(new Date());
+    const api = useContext(WixAPIContext);
+    const { slug } = useParams<RouteParams['/lesson/:slug']>();
+    const { data: lessonData } = useLessonBySlug(slug);
+    const { data: availability, isLoading } = useAvailability(lessonData?._id!);
+
+
+    const calendarDataByDate = availability?.availabilityEntries.reduce<{ [key: string]: SlotAvailability[] }>((accumulator, lesson) => {
+        const lessonDate = lesson.slot?.endDate
+            ? format(parseISO(lesson.slot.endDate), "dd/MM/yyyy")
+            : "Invalid date";
+        if (lessonDate in accumulator) {
+            accumulator[lessonDate].push(lesson);
+        } else {
+            accumulator[lessonDate] = [lesson];
+        }
+        return accumulator;
+    }, {});
 
     const renderDayContents = (day: number, date: Date) => {
         const dateString = format(date, 'dd/MM/yyyy');
-        const isAvailable = dateString in data && date >= today;
+        const isAvailable = dateString in (calendarDataByDate || {}) && date >= today;
         const isSelected = format(date, 'dd/MM/yyyy') === format(selectedDate, 'dd/MM/yyyy');
         return <CalendarDate isAvailable={isAvailable} date={day} isSelected={isSelected} />;
     };
