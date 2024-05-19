552a295956d399a4d44029a915cb4d540b2ec244
YuriyMorkovnik <yurim@wix.com>
2024-05-19 13:34:08 +0300

    payment page changes

--- src/components/checkout/checkout.tsx
+++ src/components/checkout/checkout.tsx
@@ -1,7 +1,6 @@
 import classNames from 'classnames';
 import styles from './checkout.module.scss';
 import commonStyles from '@styles/common-styles.module.scss';
-import { SingleInput } from '../single-input/single-input';
 import CommonStyles_module from '../../styles/common-styles.module.scss';
 
 export interface CheckoutProps {
@@ -12,46 +11,49 @@ export const Checkout = ({ className }: CheckoutProps) => {
     return (
         <div className={classNames(styles.root, className)}>
             <h6 className={styles.title}>Customer details </h6>
-            <label className={commonStyles['input-label']} htmlFor="email">
-                Email
-                <input
-                    id="email"
-                    type="email"
-                    required
-                    aria-required
-                    pattern="^.+@.+\.[a-zA-Z]{2,63}$"
-                    name="email"
-                    maxLength={150}
-                />
-            </label>
-            <label className={commonStyles['input-label']} htmlFor="first-name">
-                First name
-                <input
-                    id="first-name"
-                    type="text"
-                    aria-required
-                    required
-                    name="name"
-                    maxLength={100}
-                />
-            </label>
-            <label className={commonStyles['input-label']} htmlFor="last-name">
-                Last name
-                <input id="last-name" type="text" name="lastName" maxLength={150} />
-            </label>
-            <label className={commonStyles['input-label']} htmlFor="phone">
-                Phone
-                <input id="phone" type="phone" name="phone" maxLength={100} />
-            </label>
-            <label className={commonStyles['input-label']} htmlFor="phone">
-                Add your message
-                <input id="message" type="text" name="message" maxLength={200} />
-            </label>
-            <button
-                className={classNames(CommonStyles_module.primaryButton, styles['checkout-button'])}
-            >
-                Continue
-            </button>
+            <form>
+                <label className={commonStyles['input-label']} htmlFor="email">
+                    Email
+                    <input
+                        id="email"
+                        type="email"
+                        required
+                        aria-required
+                        pattern="^.+@.+\.[a-zA-Z]{2,63}$"
+                        name="email"
+                        maxLength={150}
+                    />
+                </label>
+                <label className={commonStyles['input-label']} htmlFor="first-name">
+                    First name
+                    <input
+                        id="first-name"
+                        type="text"
+                        aria-required
+                        required
+                        name="name"
+                        maxLength={100}
+                    />
+                </label>
+                <label className={commonStyles['input-label']} htmlFor="last-name">
+                    Last name
+                    <input id="last-name" type="text" name="lastName" maxLength={150} />
+                </label>
+                <label className={commonStyles['input-label']} htmlFor="phone">
+                    Phone
+                    <input id="phone" type="phone" name="phone" maxLength={100} />
+                </label>
+                <label className={commonStyles['input-label']} htmlFor="phone">
+                    Add your message
+                    <input id="message" type="text" name="message" maxLength={200} />
+                </label>
+                <button
+                    type='submit'
+                    className={classNames(CommonStyles_module.primaryButton, styles['checkout-button'])}
+                >
+                    Continue
+                </button>
+            </form>
         </div>
     );
 };
--- src/components/payment/payment.tsx
+++ src/components/payment/payment.tsx
@@ -1,7 +1,6 @@
 import classNames from 'classnames';
 import styles from './payment.module.scss';
-import commonStyles from '@styles/common-styles.module.scss';
-import { SingleInput } from '../single-input/single-input';
+import commonStyles from '../../styles/common-styles.module.scss';
 
 export interface PaymentProps {
     className?: string;
@@ -10,23 +9,67 @@ export interface PaymentProps {
 export const Payment = ({ className }: PaymentProps) => {
     return (
         <div className={classNames(styles.root, className, styles.paymentdiv)}>
-            <h3 className={styles.title}>Payment</h3>
-            <h2 className={styles.boldMessage}>
-                We&apos;re not able to accept online payments at the moment.
-            </h2>
-            <h3>Please contact us to complete your purchase. Thanks. </h3>
-            <h2 className={styles.subTitle}>Billing address</h2>
-            <SingleInput inputTitle="First name" />
-            <SingleInput inputTitle="Last name" />
-            <SingleInput inputTitle="Phone" />
-            <SingleInput inputTitle="Country/Region" />
-            <SingleInput inputTitle="Address" />
-            <SingleInput inputTitle="City" />
-            <SingleInput inputTitle="State" />
-            <SingleInput inputTitle="Zip / Postal code" />
-            <h2 className={styles.subTitle}>Review &amp; place order</h2>
-            <h3>Review the order details above, and place your order when you’re ready.</h3>
-            <button className={styles.btnPayment}>Place Order &amp; Pay</button>
+            <div className={styles['title-wrapper']}>
+                <h3 className={styles.title}>Payment</h3>
+                <span className={styles.boldMessage}>
+                    We&apos;re not able to accept online payments at the moment.
+                </span>
+                <span>Please contact us to complete your purchase. Thanks. </span>
+                <span className={styles.subTitle}>Billing address</span>
+            </div>
+            <form>
+                <label className={commonStyles['input-label']} htmlFor="first-name">
+                    First name
+                    <input
+                        id="first-name"
+                        type="text"
+                        aria-required
+                        required
+                        name="name"
+                        maxLength={100}
+                    />
+                </label>
+                <label className={commonStyles['input-label']} htmlFor="last-name">
+                    Last name
+                    <input id="last-name" type="text" name="lastName" maxLength={150} />
+                </label>
+                <label className={commonStyles['input-label']} htmlFor="phone">
+                    Phone
+                    <input id="phone" type="phone" name="phone" maxLength={100} />
+                </label>
+                <label className={commonStyles['input-label']} htmlFor="country">
+                    Country/Region
+                    <input id="country" type="text" name="country" maxLength={150} />
+                </label>
+                <label className={commonStyles['input-label']} htmlFor="address">
+                    Address
+                    <input id="address" type="text" name="address" maxLength={150} />
+                </label>
+                <label className={commonStyles['input-label']} htmlFor="city">
+                    City
+                    <input id="city" type="text" name="city" maxLength={150} />
+                </label>
+                <label className={commonStyles['input-label']} htmlFor="state">
+                    State
+                    <input id="state" type="text" name="state" maxLength={150} />
+                </label>
+                <label className={commonStyles['input-label']} htmlFor="zip">
+                    Zip / Postal code
+                    <input id="zip" type="text" name="zip" maxLength={150} />
+                </label>
+                <div className={styles['info-wrapper']}>
+                    <span className={styles.subTitle}>Review &amp; place order</span>
+                    <span>
+                        Review the order details above, and place your order when you’re ready.
+                    </span>
+                </div>
+                <button
+                    type="submit"
+                    className={classNames(commonStyles.primaryButton, styles['submit-button'])}
+                >
+                    Place Order &amp; Pay
+                </button>
+            </form>
         </div>
     );
 };
