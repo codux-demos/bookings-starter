934479cbfeb2bed491f3ca52f757cf5e47d746d2
Daniel Cohen <danielcoh@wix.com>
2024-05-26 14:50:47 +0300

    created the Calendar component

--- src/components/calendar/calendar.tsx
+++ src/components/calendar/calendar.tsx
@@ -1,19 +1,53 @@
-import classNames from 'classnames';
-import styles from './calendar.module.scss';
-import commonStyles from '@styles/common-styles.module.scss';
+import React from 'react';
 import { DayPicker } from 'react-day-picker';
-import "react-day-picker/dist/style.css";
+import 'react-day-picker/lib/style.css';
+import { format, isBefore, isSameDay } from 'date-fns';
+import { CalendarDate } from '../calendar-date/calendar-date';
+import './daypickerstyles.css';
 
-
-export interface CalendarProps {
+interface CalendarComponentProps {
     selectedDate: Date;
     setSelectedDate: (date: Date) => void;
+    availableDates: string[];
 }
 
-export const Calendar = ({ selectedDate, setSelectedDate }: CalendarProps) => {
-    return <DayPicker 
-    mode="single"
-    selected = {selectedDate}
-    onSelect={setSelectedDate}
-/>;
+export const Calendar: React.FC<CalendarComponentProps> = ({ selectedDate, setSelectedDate, availableDates }) => {
+    const today = new Date();
+
+    const handleDayClick = (date: Date) => {
+        setSelectedDate(date);
+    };
+
+    const renderDay = (date: Date) => {
+        const dateString = format(date, 'dd/MM/yyyy');
+        const isDayPassed = isBefore(date, today);
+        const isAvailable = availableDates.includes(dateString) && !isDayPassed;
+        const isSelected = isSameDay(date, selectedDate);
+
+        return (
+            <CalendarDate
+                isAvailable={isAvailable}
+                date={date.getDate()}
+                isSelected={isSelected}
+                isDayPassed={isDayPassed}
+            />
+        );
+    };
+
+    return (
+        <DayPicker
+            selected={selectedDate}
+            onDayClick={handleDayClick}
+            modifiers={{
+                available: (date) => availableDates.includes(format(date, 'dd/MM/yyyy')),
+            }}
+            modifiersStyles={{
+                available: {
+                    color: 'black',
+                    backgroundColor: 'orange',
+                },
+            }}
+            renderDay={renderDay}
+        />
+    );
 };
