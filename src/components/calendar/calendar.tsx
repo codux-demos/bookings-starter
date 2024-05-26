addae3ceb3457972045cf1d70d1fd0b76e1b9212
Daniel Cohen <danielcoh@wix.com>
2024-05-26 14:28:54 +0300

    fixed the style file import

--- src/components/calendar/calendar.tsx
+++ src/components/calendar/calendar.tsx
@@ -2,7 +2,7 @@ import classNames from 'classnames';
 import styles from './calendar.module.scss';
 import commonStyles from '@styles/common-styles.module.scss';
 import { DayPicker } from 'react-day-picker';
-import "react-day-picker/style.css";
+import "react-day-picker/dstyle.css";
 
 
 export interface CalendarProps {
